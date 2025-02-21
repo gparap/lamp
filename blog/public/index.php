c5bfa37ecfb1096d1433305691d54767d8f3b400
gparap <gparap@protonmail.com>
2025-02-21 10:32:34 +0200

    Create main index page to display live post

--- blog/public/index.php
+++ blog/public/index.php
@@ -17,7 +17,7 @@
 
 	<!-- Navigation -->
 	<nav class="navbar navbar-expand-lg bg-body-tertiary">
-		<div class="container-fluid">
+		<div class="container">
 			<a class="navbar-brand" href="#">Blog</a>
 			<button class="navbar-toggler" type="button"
 				data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -45,11 +45,58 @@
 		</div>
 	</nav>
 
+	<!-- Content -->
 	<main>
-		<p class="h1 text-center mt-5">Welcome to blog!</p>
-		<p class="text-center">
-			<img alt="" src="img/static/logo.png">
-		</p>
+		<div class="container">
+		<?php
+            $is_welcome_page = true;
+            
+            // Display post
+            if (isset($_GET['id'])) {
+                // connect to database
+                require_once ($_SERVER['DOCUMENT_ROOT'] . '/blog/admin/src/utils/functions.php');
+                $connection = connectToDatabase();
+            
+                // init vars
+                $id = $_GET['id'];
+                $title = "";
+                $content = "";
+                $author = "";
+                $date = "";
+                $image = "";
+            
+                // get post
+                $query = "SELECT * FROM `posts` WHERE id='$id'";
+                $result = mysqli_query($connection, $query);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $title = $row['title'];
+                    $author = $row['author'];
+                    $content = $row['content'];
+                    $image = $row['image'];
+                    $date = $row['date'];
+                }
+            
+                // display post
+                if (! empty($title)) {
+                    echo '<div class="mt-5 mb-0"><label><p class="h1">' . $title . '</p>&nbsp;</label></div><BR>';
+                    if (! empty($image)) {
+                        echo '<div class="mb-3"><img src="img/' . $image . '" width="256" height="128"></div>';
+                    }
+                    echo '<div class="mt-0 mb-0"><label><i><p class="small">by&nbsp;' . $author . ',&nbsp;published on&nbsp;' . $date . '</p></i></label></div><BR>';
+                    echo '<div class="mt-5 mb-0"><label><p class="fs-5">' . $content . '</p></label></div><BR>';
+            
+                    // do not display welcome page
+                    $is_welcome_page = false;
+                }
+            }
+            
+            // Display welcome page
+            if ($is_welcome_page) {
+                echo '<p class="h1 text-center mt-5">Welcome to blog!</p>
+                                            <p class="text-center"><img alt="" src="img/static/logo.png"></p>';
+            }
+            ?>
+        </div>
 	</main>
 
 	<!-- Scripts -->
