6256fbc495a7de0de7688f2bfd478ee9948231dc
gparap <54770777+gparap@users.noreply.github.com>
2025-12-03 14:19:46 +0200

    Create functionality for displaying books in main

--- e-library/public/books.php
+++ e-library/public/books.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - e-library</title>
+    <title>Books - e-library</title>
     <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i&amp;display=swap">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg fixed-top bg-body clean-navbar">
+	<nav class="navbar navbar-expand-lg fixed-top bg-body clean-navbar">
         <div class="container"><a class="navbar-brand logo" href="#">e-library</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav ms-auto">
@@ -23,21 +23,33 @@
             </div>
         </div>
     </nav>
-    <main class="page">
-        <section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/bg.jpg&quot;);color:rgba(9, 162, 255, 0.85);">
-            <div class="text">
-                <h2>Welcome to e-library!</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                <a class="btn btn-outline-light btn-lg" type="button" href="books.php">View Books</a>
-            </div>
-        </section>
-    </main>
-    <footer class="page-footer dark">
-        <div class="footer-copyright mt-0">
-            <p>© 2025 gparap</p>
-        </div>
-    </footer>
-    <script src="assets/js/bootstrap.min.js"></script>
+	<main class="page">
+		<section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/bg.jpg&quot;);color:rgba(9, 162, 255, 0.85);">
+			<div class="row justify-content-center"	style="z-index: 1; padding: 1.5rem 1.5rem 0 1.5rem;">
+
+ 				<!--TODO: Fetch books here -->
+				<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+					<div class="card h-100">
+						<img src="..." class="card-img-top img-fluid" alt="TODO: image">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title">Lorem ipsum</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit. Praesent justo augue, pretium quis sodales
+								quis, eleifend at nunc.</p>
+							<a href="#" class="btn btn-primary mt-auto">TODO: action</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</section>
+	</main>
+	<footer class="page-footer dark">
+		<div class="footer-copyright mt-0">
+			<p>© 2025 gparap</p>
+		</div>
+	</footer>
+	<script src="assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
