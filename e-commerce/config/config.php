1e2eff15d9f0009fa8d37bf521a82e69077fcb10
gparap <gparap@protonmail.com>
2026-09-15 15:05:25 +0300

    Create login functionality
    Use temp views for admins & users

--- e-commerce/config/config.php
+++ e-commerce/config/config.php
@@ -1,14 +1,18 @@
 <?php
 
-/*
+/**
  * https://mit-license.org
- * Copyright © 2023 gparap
- * Database configuration.
+ * Copyright © 2026 gparap
+ * Configuration constants.
  */
 
- define('HOST', 'localhost');
- define('DATABASE', 'e_commerce_db');
- define('USER', 'root');
- define('PASSWORD', '');
+define('HOST', 'localhost');
+define('DATABASE', 'e_commerce_db');
+define('USER', 'root');
+define('PASSWORD', '');
 
-?>
+define('ROOT_PATH', realpath(__DIR__ . '/..') . '/');
+define('UTILS_PATH', ROOT_PATH . '/src/utils/');
+define('INCLUDES_PATH', ROOT_PATH . '/src/includes/');
+define('ADMIN_URL', 'https://localhost/e-commerce/public/admin/');
+define('USER_URL', 'https://localhost/e-commerce/public/user/');
--- e-commerce/src/utils/functions.php
+++ e-commerce/src/utils/functions.php
@@ -1,41 +1,45 @@
 <?php
 
-/*
+/**
  * https://mit-license.org
- * Copyright © 2023 gparap
- * Connect to database.
+ * Copyright © 2026 gparap
  */
 
-//get category name based on category id
-function get_category_name($id) {
-    switch ($id) {
-        case 1:
-            $category_name = "auto";
-            break;
-
-        case 2:
-            $category_name = "bikes";
-            break;
-        
-        case 3:
-            $category_name = "drinks";
-            break;
-
-        case 4:
-            $category_name = "garden";
-            break;
-
-        case 5:
-            $category_name = "gym";
-            break;
-
-        case 6:
-            $category_name = "tech";
-            break;
-
-        default:
-            break;
-    }
-    return $category_name;
+/* Gets the database connection object. */
+function get_db_connection(): mysqli {
+	require_once (__DIR__ . '/connection.php');
+	return $db_connection;
 }
-                                
+
+/* Logging users in & give them session vars. */
+function log_in_user($username, $password): bool {
+	$is_user_logged_in = false;
+
+	//perform the log in process
+	$db_connection = get_db_connection ();
+	$query = "SELECT * FROM `users` WHERE username = ?";
+	$stmt = mysqli_prepare ( $db_connection, $query );
+	mysqli_stmt_bind_param ( $stmt, "s", $username );
+	mysqli_stmt_execute ( $stmt );
+	$query_results = mysqli_stmt_get_result ( $stmt );
+	$query_results_rows = mysqli_num_rows ( $query_results );
+	if ($query_results_rows == 1) {
+		while ( $row = mysqli_fetch_assoc ( $query_results ) ) {
+			if (password_verify ( $password, $row ['password'] )) {
+				//give the user session vars
+				if (session_status() == PHP_SESSION_NONE) {
+					session_start ();				
+				}
+				$_SESSION ['id'] = $row ['id'];
+				$_SESSION ['role'] = $row ['role'];
+				$_SESSION ['name'] = $row ['username'];
+
+				$is_user_logged_in = true;
+			}
+		}
+	}
+	$db_connection->close();
+	return $is_user_logged_in;
+}
+
+?>
